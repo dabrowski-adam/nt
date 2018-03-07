df93819c8aee3d838b055e95c1075ca021934812
Adam Dąbrowski <contact@adamdabrowski.com>
2018-03-07 21:35:39 +0100

    Add templating

--- index.mjs
+++ index.mjs
@@ -1,19 +1,29 @@
-import fs from 'fs-extra';
-import md from 'showdown';
+import fs  from 'fs-extra';
+import md  from 'showdown';
+import tpl from 'mustache';
 
-const contentDir = './content/';
-const outputDir	 = './build/';
+const contentDir  = './content/';
+const templateDir = './templates/';
+const outputDir	  = './build/';
 
 (async () => {
 	try {
+		// Parse markdown files
 		const fileNames	   = await fs.readdir(contentDir);
 		const fileContents = await Promise.all(fileNames.map(f => fs.readFile(contentDir + f, 'utf8')));
 		const converter    = new md.Converter();
-		const converted    = fileContents.map(x => converter.makeHtml(x.toString()));
-		converted.forEach((x, i) => { 
+		const converted    = fileContents.map(x => converter.makeHtml(x));
+		
+		/*converted.forEach((x, i) => { 
 			fs.outputFile(outputDir + fileNames[i].slice(0, -3) + '.html', x, 'utf8');
-		});
+		});*/
+
+		// Render mustache template
+		const template = await fs.readFile(templateDir + 'index.html', 'utf8');
+		const rendered = tpl.render(template, { reports: converted });
+		fs.outputFile(outputDir + 'index.html', rendered, 'utf8');
+
 	} catch (e) {
-		console.error(' Parsing markdown failed with:\n', e);
+		console.error(' Build failed with:\n', e);
 	}
 })();
